79a137964d9c1f792cb985ee98b7e520899bcc9b
Jimin0304 <pjws0304@naver.com>
2023-10-09 18:37:21 +0900

    chore: 에러 발생 시 바로 return하도록 수정

--- commands/user.cpp
+++ commands/user.cpp
@@ -5,9 +5,9 @@ void Server::user(Client *client, Message *msg)
 	if (!client->hasValidPassword()) // PASS 틀린 경우, 없는 경우 
 		return ;
 	if (msg->params.size() < 4)
-		client->sendMsg(ERR_NEEDMOREPARAMS(client->getNickname(), msg->command));
+		return (client->sendMsg(ERR_NEEDMOREPARAMS(client->getNickname(), msg->command)));
 	if (client->isRegistered())
-		client->sendMsg(ERR_ALREADYREGISTRED(client->getNickname()));
+		return (client->sendMsg(ERR_ALREADYREGISTRED(client->getNickname())));
 
 	client->setUsername(msg->params[1]);
 	client->setHostname(msg->params[2]);
