264b23c5819f1e694869e91ad64d24c861a4ff8a
heeeete <mkoui@naver.com>
2023-10-10 02:40:10 +0900

    mode 수정

--- commands/mode.cpp
+++ commands/mode.cpp
@@ -18,6 +18,17 @@ static bool	isInt(std::string input)
 	return false;
 }
 
+void	parseModes(std::string& modes) {
+	if (modes.size() == 1)
+		return modes.clear();
+	if (modes.size() == 2 && !isalpha(modes.at(0)) && !isalpha(modes.at(1)))
+		return modes.clear();
+	if (modes[modes.size() - 1] == '+' || modes[modes.size() - 1] == '-')
+		modes.erase(modes.size() - 1);
+	else if (modes.size() == 3 && !isalpha(modes.at(0)) && !isalpha(modes.at(1)))
+		modes.erase(0, 1);
+}
+
 void Server::channelModes(Client *client, Message *msg) {
 	std::string nick = client->getNickname();
 	std::string channelName = msg->params[0];
@@ -32,103 +43,113 @@ void Server::channelModes(Client *client, Message *msg) {
 		return (client->sendMsg(ERR_CHANOPRIVSNEEDED(nick, channelName)));
 
 	size_t arg_n = 2;
-	size_t cnt = 0;
 	char sign = modes[0];
+	std::string set;
 	std::string args;
 	std::string error;
 	std::string::iterator isBegin = modes.begin() + 1;	// +nt 면 n 부터 시작
 	Client *oper = NULL;
+	if (modes[0] == '-' || modes[0] == '+')
+		set += modes[0];
 	while (isBegin != modes.end()) {
-		if ((*isBegin) == '+')
-			sign = '+';
-		else if ((*isBegin) == '-')
-			sign = '-';
+		if ((*isBegin) == '+' || (*isBegin) == '-'){
+			sign = (*isBegin);
+			set += (*isBegin);
+		}
 		switch ((*isBegin)) {
 			case 'n':
-				++cnt;
+				if (sign == '+' && ch->setMode('n') != -1)
+					set += "n";
+				else if (sign == '-' && ch->unSetMode('n') != -1)
+					set += "n";
 				break;
 			case 't':
-				++cnt;
-				if (sign == '+')
-					ch->setMode('t');
-				else if (sign == '-')
-					ch->unSetMode('t');
+				if (sign == '+' && ch->setMode('t') != -1)
+					set += "t";
+				else if (sign == '-' && ch->unSetMode('t') != -1)
+					set += "t";
 				break;
 			case '+':
 				break;
 			case '-':
 				break;
 			case 'i':
-				if (sign == '+')
-					ch->setMode('i');
-				else if (sign == '-')
-					ch->unSetMode('i');
-				++cnt;
+				if (sign == '+' && ch->setMode('i') != -1)
+					set += "i";
+				else if (sign == '-' && ch->unSetMode('i') != -1)
+					set += "i";
 				break;
 			case 'l':
 				if (sign == '+')
 				{
-					if (msg->params.size() < arg_n + 1)
+					if (msg->params.size() < arg_n + 1) {
 						client->sendMsg(ERR_NOLIMIT(nick, channelName));
+						break;
+					}
 					else if (isInt(msg->params[arg_n])){
 						ch->setUserLimit(msg->params[arg_n]);
 						args += msg->params[arg_n] + " ";
 						++arg_n;
+						set += "l";
 						ch->setMode('l');
 					}
 				}
-				else if (sign == '-')
-					ch->unSetMode('l');
-				++cnt;
+				else if (sign == '-' && ch->unSetMode('l') != -1)
+					set += "l";
 				break;
 			case 'o':
-				if (msg->params.size() < arg_n + 1)
-				{
+				if (msg->params.size() < arg_n + 1){
 					client->sendMsg(ERR_NONICK(nick,channelName));
 					break;
 				}
 				oper = getClient((msg->params[arg_n]));
-				if (!oper)
+				if (!oper){
 					client->sendMsg(ERR_NOSUCHNICK(nick, msg->params[arg_n]));
-				else if (!ch->hasClient(oper))
+					break;
+				}
+				if (!ch->hasClient(oper)){
 					client->sendMsg(ERR_USERNOTINCHANNEL(nick, oper->getNickname(), channelName));
-				else if (sign == '+')
-					ch->addOperator(oper);
-				else if (sign == '-')
-					ch->removeOperator(oper);
+					break;
+				}
+				else if (sign == '+' && ch->addOperator(oper))
+					set += "o";
+				else if (sign == '-' && ch->removeOperator(oper))
+					set += "o";
 				args += msg->params[arg_n] + " ";
-				++cnt;
 				++arg_n;
 				break;
 			case 'k':
 				if (msg->params.size() < arg_n + 1)
-					client->sendMsg(ERR_NONICK(nick,channelName));
-				else if (sign == '+'){
-					ch->setMode('k');
+				{
+					client->sendMsg(ERR_NOKEY(nick,channelName));
+					break;
+				}
+				else if (sign == '+' && ch->setMode('k') != -1){
+					set += "k";
 					ch->setChannelPassword(msg->params[arg_n]);
 					args += msg->params[arg_n] + " ";
-					++arg_n;
 				}
-				else if (sign == '-')
-					ch->unSetMode('k');
-				++cnt;
+				else if (sign == '-' && ch->unSetMode('k') != -1)
+					set += "k";
+				++arg_n;
 				break;
 			default:
 				error = *isBegin;
-				modes.erase(modes.find(error, 1), 1);
 				client->sendMsg(ERR_UNKNOWNMODE(nick, error));
-				continue;
 		}
 		isBegin++;
 	}
-	std::string	temp;
-	if (args.empty() && cnt)
-		temp = RPL_MODE_NOARG(client->getNickname(), client->getUsername(), client->getHostname(), channelName, modes);
-	else if (cnt)
-		temp = RPL_MODE(client->getNickname(), client->getUsername(), client->getHostname(), channelName, modes, args);
-	if (cnt){
-		client->sendMsgToChannel(temp, ch);
-		client->sendMsg(temp);
+
+	std::string	sendMsg;
+	parseModes(set);
+	std::cout << "MODE ================  " << set << "\n";
+	if (args.empty() && !set.empty())
+		sendMsg = RPL_MODE_NOARG(client->getNickname(), client->getUsername(), client->getHostname(), channelName, set);
+	else if (!set.empty())
+		sendMsg = RPL_MODE(client->getNickname(), client->getUsername(), client->getHostname(), channelName, set, args);
+	if (!set.empty()){
+		client->sendMsgToChannel(sendMsg, ch);
+		client->sendMsg(sendMsg);
 	}
 }
 
